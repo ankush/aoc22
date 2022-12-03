7653d2cab37fa70bbb6e847edbc87e0bb4f6acf1
Ankush Menat <ankushmenat@gmail.com>
2022-12-03 22:35:00 +0530

    day3 - part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,8 @@
+use std::vec;
+
 fn main() {
     println!("Sum of common priorities: {}", part1(INPUT));
+    println!("Sum of common priorities from triplets: {}", part2(INPUT));
 }
 
 fn part1(input: &str) -> i32 {
@@ -7,13 +10,33 @@ fn part1(input: &str) -> i32 {
 
     rucksacks
         .iter()
-        .map(|r| find_common_character(r.left, r.right))
+        .map(|r| find_common_character(r.compartments()))
         .map(get_priority)
         .sum()
 }
 
-fn find_common_character(a: &str, b: &str) -> char {
-    a.chars().filter(|c| b.contains(*c)).next().unwrap()
+fn part2(input: &str) -> i32 {
+    let rucksacks = get_rucksacks(input);
+    let all_supplies: Vec<&str> = rucksacks.iter().map(|r| r.supplies).collect();
+
+    all_supplies
+        .chunks(3)
+        .map(|chunk| chunk.to_vec())
+        .map(find_common_character)
+        .map(get_priority)
+        .sum()
+}
+
+fn find_common_character(strings: Vec<&str>) -> char {
+    'outer: for c in strings[0].chars() {
+        for string in strings[1..].iter() {
+            if !(string.contains(c)) {
+                continue 'outer;
+            }
+        }
+        return c;
+    }
+    panic!("Didn't find common character");
 }
 
 fn get_priority(c: char) -> i32 {
@@ -28,17 +51,23 @@ fn get_priority(c: char) -> i32 {
     }
 }
 
+#[derive(Copy, Clone)]
 struct Rucksack<'a> {
-    left: &'a str,
-    right: &'a str,
+    supplies: &'a str,
+}
+
+impl Rucksack<'_> {
+    fn compartments(&self) -> Vec<&str> {
+        let (left, right) = self.supplies.split_at(self.supplies.len() / 2);
+        vec![left, right]
+    }
 }
 
 fn get_rucksacks(input: &str) -> Vec<Rucksack> {
     let mut rucksacks = vec![];
 
     for line in input.lines() {
-        let (left, right) = line.split_at(line.len() / 2);
-        rucksacks.push(Rucksack { left, right })
+        rucksacks.push(Rucksack { supplies: line });
     }
 
     rucksacks
@@ -67,7 +96,7 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
             .zip(expected_chars)
             .zip(expected_priorities)
         {
-            let actual_common_char = find_common_character(rucksack.right, rucksack.left);
+            let actual_common_char = find_common_character(rucksack.compartments());
 
             assert_eq!(actual_common_char, exp_common_char);
             assert_eq!(get_priority(actual_common_char), expected_priority);
@@ -78,6 +107,12 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     fn part1_works() {
         assert_eq!(part1(""), 0);
         assert_eq!(part1(TEST_INPUT), 157);
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2(""), 0);
+        assert_eq!(part2(TEST_INPUT), 70);
     }
 }
 
