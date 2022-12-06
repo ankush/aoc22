7978a73dbf6bc7a78e1b6c6ff6cf1045eb5d9efb
Ankush Menat <ankushmenat@gmail.com>
2022-12-06 11:47:07 +0530

    day6 - part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,23 +1,36 @@
 fn main() {
     let input = std::fs::read_to_string("./src/input.in").unwrap();
     println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
 
 fn part1(input: &str) -> i32 {
-    for i in 4..input.len() {
-        let last_section = &input[i - 4..i];
+    find_consecutive_chars(&input, 4)
+}
+
+fn part2(input: &str) -> i32 {
+    find_consecutive_chars(&input, 14)
+}
+
+fn find_consecutive_chars(input: &str, n: usize) -> i32 {
+    for i in n..input.len() {
+        let last_section = &input[i - n..i];
         if unique(last_section) {
             return i as i32;
         }
     }
-
     panic!("Didn't find any");
 }
 
 fn unique(string: &str) -> bool {
     string
         .char_indices()
-        .find_map(|(i, c)| string.chars().skip(i + 1).find(|x| *x == c))
+        .find_map(|(i, c)| {
+            string
+                .chars()
+                .skip(i + 1)
+                .find(|x| *x == c)
+        })
         .is_none()
 }
 
@@ -32,5 +45,14 @@ mod tests {
         assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
         assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
         assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(part2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(part2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
     }
 }
